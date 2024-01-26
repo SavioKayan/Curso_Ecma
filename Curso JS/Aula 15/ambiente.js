aab7cc1f71e95f8e829215cdc5c42052876e8f03
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-26 19:43:32 -0300

    att

---
+++ Curso JS/Aula 15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [2, 8, 5, 7, 6, 9 ]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do array é ${num[0]}.`)
+
+/*
+num[5] = 11
+num.push(0)
+num.length
+num.sort
+*/
+
