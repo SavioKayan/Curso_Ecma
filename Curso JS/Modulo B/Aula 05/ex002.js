73639ff1dd6c836a096effb09e516815a2f88462
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-27 21:24:21 -0300

    Update ex002.js

--- Curso JS/Modulo B/Aula 05/ex002.js
+++ Curso JS/Modulo B/Aula 05/ex002.js
@@ -1,5 +1,7 @@
 //Manipulando e criando variáveis/
+
 //Regras//
+
 /*Podem começar em letra,$ ou _.
 Não podem começar com números.
 É possível usar letras e números.
@@ -10,35 +12,8 @@ Não podem ser palavras reservadas.*/
 //-----DICAS PARA CRIAÇÃO DE INDENTIFICADORES------//
 /*Maiúculas e minúsculas fazem diferença.
 Tente escolher nomes coerentes.
-Evite usar somente letras e numeros isolados, atribua nomes para tornar o entendimento mais simples e intuitivo.
+Evite usar somente letras e numeros isolados, atribua nomes para tornar o entendimento mais simples e intuitivo.*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-'Oi'
-var nome = 'Sávio'
-nome
-'nome'
-nome = 'Kayan'> 
-var n1 = 8
-
-var n2 = 5
-
-n1 
-8
-n2
-5
- n1 + n2
-13
 
 
 
