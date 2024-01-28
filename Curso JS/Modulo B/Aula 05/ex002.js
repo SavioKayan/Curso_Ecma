8e2e65af7cfc731bdef22fd7b742e389d48d38a4
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-27 21:47:41 -0300

    up

--- Curso JS/Modulo B/Aula 05/ex002.js
+++ Curso JS/Modulo B/Aula 05/ex002.js
@@ -12,7 +12,20 @@ Não podem ser palavras reservadas.*/
 //-----DICAS PARA CRIAÇÃO DE INDENTIFICADORES------//
 /*Maiúculas e minúsculas fazem diferença.
 Tente escolher nomes coerentes.
-Evite usar somente letras e numeros isolados, atribua nomes para tornar o entendimento mais simples e intuitivo.*/
+Evite usar somente letras e numeros isolados, atribua nomes para tornar o entendimento mais simples e intuitivo.
+Ex: var nome = 'nome'
+var tel = 'tel'
+Variáveis podem guardar numéros inteiros(int), números décimais(float, ponto flutuante) que são do tipo Number(Número), o NaN significa o Not a Number, não é um número, também temos o infinity.
+Também podem guarda strings(conjunto de caracteres compostos por numeros, letras e simbolos)
+e também do tipo booleano(true ou false).
+null que é um objeto.
+undefined
+object e function
+type of mostra o tipo da variável.
+
+
+*/
+
 
 
 
