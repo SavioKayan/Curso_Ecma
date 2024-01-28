709b0377dc2064ed0c7bcef86773910b58f96292
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-28 19:18:58 -0300

    desafio 02 concluido.

---
+++ Curso JS/desafios/d002/d002.js
@@ -0,0 +1,5 @@
+function btn01() {
+  var nome = window.prompt('Qual seu nome?')
+  var idade = window.prompt(`Olá, ${nome}! Quanto anos você tem?`)
+  window.alert(`Acabei de conhcer ${nome}, que tem ${idade} anos de idade.`)
+}
