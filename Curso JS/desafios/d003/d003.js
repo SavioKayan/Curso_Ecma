5dcee4dda4e714dec43001b61e23100f397ce359
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-28 19:34:49 -0300

    desafio 003 Concluido.

--- Curso JS/desafios/d003/d003.js
+++ Curso JS/desafios/d003/d003.js
@@ -1,3 +1,6 @@
 function btn01() {
-  var n1 = window.prompt("Digite um número inteiro qualquer")
+  var n1 = Number(window.prompt("Digite um número inteiro qualquer"))
+  var sucessor = n1 + 1
+  var antecessor = n1 - 1
+  window.alert(`O sucessor de ${n1} é ${sucessor} e o antecessor de ${n1} é ${antecessor}.`)
 }
