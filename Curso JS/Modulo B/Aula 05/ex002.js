7592128bbc92cf3ae39ea722c1771e7e4c9ca8e2
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-27 22:24:33 -0300

    update

--- Curso JS/Modulo B/Aula 05/ex002.js
+++ Curso JS/Modulo B/Aula 05/ex002.js
@@ -2,7 +2,11 @@
 
 //Regras//
 
-/*Podem começar em letra,$ ou _.
+/*
+Variável é um espaço reservado na memória do computador que serve para armazrnar um tipo de informação, recebdno um valor de cada vez e não podendo conter valores smultâneos.
+Para declarar ariáveis em JS usamos var ou let e depois o nome da variável, logo em seguida lhe atribuindo um valor inteiro como 1,2,3,4,5,6 ou real como 0.5, 1.2, 1.0, 5,4.
+
+Podem começar em letra,$ ou _.
 Não podem começar com números.
 É possível usar letras e números.
 É possível usar acentos e simbolos.
@@ -15,10 +19,10 @@ Tente escolher nomes coerentes.
 Evite usar somente letras e numeros isolados, atribua nomes para tornar o entendimento mais simples e intuitivo.
 Ex: var nome = 'nome'
 var tel = 'tel'
-Variáveis podem guardar numéros inteiros(int), números décimais(float, ponto flutuante) que são do tipo Number(Número), o NaN significa o Not a Number, não é um número, também temos o infinity.
+Variáveis(STRING, NUMBER, BOOLEAN,NULL,UNDEFINED,SYMBOL) podem guardar numéros inteiros(int), números décimais(float, ponto flutuante) que são do tipo Number(Número), o NaN significa o Not a Number, não é um número, também temos o infinity.
 Também podem guarda strings(conjunto de caracteres compostos por numeros, letras e simbolos)
 e também do tipo booleano(true ou false).
-null que é um objeto.
+Null signifca que a variável está sem nenhum valor atribuido, valor nulo.
 undefined
 object e function
 type of mostra o tipo da variável.
