473da61262f498178d3b23bf1fe49107ae7c6b02
Sávio Kayan Queiroz de Oliveira <saviokayan45@gmail.com>
2024-01-28 00:02:08 -0300

    update com o primeiro desafio.

---
+++ Curso JS/Modulo B/Aula 07/operadores.js
@@ -0,0 +1,28 @@
+/* O JS possui alguns tipos de operadores, sendo eles:
+aritméticos
+atribuição
+relacionais
+lógicos
+ternário
+*/
+/*Os aritméticos são:
++ = soma
+- = subtração
+* = multiplicação
+/ = divisão inteira
+% = resto da divisão
+** = exponenciação
+*/
+
+/* Os operadores de atribuição sâo:
++= | a += b |a = a + b
+-= | a -= b |a = a - b
+*= | a *= b |a = a * b
+/= | a /= b |a = a / b
+**=| a **= b |a = a ** b
+%= | a %= b | a = a % b
+
+Os operadores de encremente são:
+++ x++ x = x + 1 ou x += 1
+-- x-- x = x - 1 ou x -= 1
+*/
---
+++ Curso JS/desafios/d001.js
@@ -0,0 +1,10 @@
+function btn01() {
+  window.alert('Você clicou no primeiro botão!');
+}
+function btn02(){
+  alert('Você clicou no segundo botão!');
+}
+function btn03(){
+  alert('Você clicou no terceiro botão!');
+}
+//Function para cada botão com um alerta para aparecer na tela.
